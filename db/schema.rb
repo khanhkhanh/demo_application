3a61a7bdbbfd459d495b9d0ade1134a797c8b0ed
khanhkhanh <ngo.duy.khanh@framgia.com>
2015-05-05 10:32:36 +0700

    Finish demo app v2 :D

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150504014816) do
+ActiveRecord::Schema.define(version: 20150505032908) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "content"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150504014816) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
   end
 
   add_index "entries", ["user_id", "created_at"], name: "index_entries_on_user_id_and_created_at"
